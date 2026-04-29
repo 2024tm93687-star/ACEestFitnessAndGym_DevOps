01743a9e584b0883816377ef3543172bd4942516
2024tm93687-star <2024tm93687@wilp.bits-pilani.ac.in>
2026-04-29 18:05:50 -0500

    Update app.py

--- app.py
+++ app.py
@@ -8,6 +8,7 @@ from flask import Flask, Response, abort, jsonify, request
 
 _rng = random.SystemRandom()
 
+
 class ACEestService:
     def __init__(self):
         self.db_name = os.getenv("ACEEST_DB_NAME", "aceest_fitness.db")
